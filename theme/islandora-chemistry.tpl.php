1959be4b71ad347901e3ccf1f2bbd56347222422
Adam Vessey <adam@discoverygarden.ca>
2014-06-16 13:39:24 +0000

    Add wrapper div to markup, 'cause it seems like a good idea?

--- theme/islandora-chemistry.tpl.php
+++ theme/islandora-chemistry.tpl.php
@@ -3,6 +3,8 @@
  * @file
  */
 ?>
-<div class='islandora-chemistry-image'><?php print $image; ?></div>
-<div class='islandora-chemistry-viewer'><?php print $viewer; ?></div>
-<div class='islandora-chemistry-metadata'><?php print $metadata; ?></div>
+<div class='islandora-chemistry'>
+  <div class='image'><?php print $image; ?></div>
+  <div class='viewer'><?php print $viewer; ?></div>
+  <div class='metadata'><?php print $metadata; ?></div>
+</div>
