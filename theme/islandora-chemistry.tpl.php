62c48298b13beb29450cb6aaf0976f0ee9037177
Adam Vessey <adam@discoverygarden.ca>
2014-06-16 12:14:54 +0000

    Flesh out the object display.

--- theme/islandora-chemistry.tpl.php
+++ theme/islandora-chemistry.tpl.php
@@ -3,3 +3,6 @@
  * @file
  */
 ?>
+<div class='islandora-chemistry-image'><?php print $image; ?></div>
+<div class='islandora-chemistry-viewer'><?php print $viewer; ?></div>
+<div class='islandora-chemistry-metadata'><?php print $metadata; ?></div>
