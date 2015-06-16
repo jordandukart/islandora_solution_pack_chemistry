aa12d90dd586eda54a2a5fae466e7c3db912bcae
Jordan Dukart <jordan@discoverygarden.ca>
2015-06-16 16:07:09 -0300

    Add chemical exporter.

--- theme/islandora-chemistry.tpl.php
+++ theme/islandora-chemistry.tpl.php
@@ -7,7 +7,7 @@
   <div class='image'><?php print $image; ?></div>
   <div class='viewer'><?php print $viewer; ?></div>
   <?php if (user_access(ISLANDORA_METADATA_EDIT)): ?>
-    <div class='edit'><?php print l('edit', "islandora/edit_form/{$islandora_object->id}/MODS"); ?></div>
+    <div class='edit'><?php print l(t('edit'), "islandora/edit_form/{$islandora_object->id}/MODS"); ?></div>
   <?php endif; ?>
   <div class='metadata'><?php print $metadata; ?></div>
 </div>
